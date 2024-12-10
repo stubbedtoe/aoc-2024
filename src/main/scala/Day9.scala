efece90230f1c7f2a5d9d63705bcda3087e359db
Andrew Healy <werdnah19@gmail.com>
2024-12-10 16:33:49 +0000

    day 9 part two

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -4,10 +4,15 @@ sealed trait Fragment
 case class Empty() extends Fragment
 case class File(id: Long) extends Fragment
 
-// pt2
-case class Block(id: Long, size: Int, freeSpaceFollowing: Long)
-
 object Day9 {
+  def printFragment(fragment: Fragment): String =
+    fragment match
+      case Empty() => "."
+      case File(id) => if id > 9 then s"($id)" else id.toString()
+
+  def printBlocks(blocks: Array[Fragment]): String =
+    blocks.map(printFragment).flatten.mkString
+
   def solve(lines: List[String], part: Part): String =
     part match {
         case Part.One => part1(lines)
@@ -33,69 +38,44 @@ object Day9 {
                   moveBlocksPt1(blocks, target)
 
   @tailrec
-  def moveBlocksPt2(blocks: List[Block], blockToMove: Block): List[Block] =
-    if (blockToMove.id == 0)
+  def moveBlocksPt2(blocks: Array[Fragment], target: Long): Array[Fragment] =
+    if (target == 0)
       blocks
     else
-      // only edit the last element of this:
-      // val leftList = blocks.takeWhile(b => (b.id != blockToMove.id - 1) && (b.freeSpaceFollowing < blockToMove.size))
+      val indicesOfBlocksToMove =
+        blocks
+          .zipWithIndex
+          .filter((block, _) =>
+            block match
+              case Empty() => false
+              case File(id) => id == target
+          )
+          .map(_._2)
 
+      val numberOfBlocksToMove = indicesOfBlocksToMove.length
 
-      val blocksWithoutOneToMove = blocks.filter(b => b.id != blockToMove.id)
-      val nextBlock = blocks.find(b => b.id == (blockToMove.id - 1))
-      val leftList = blocksWithoutOneToMove.takeWhile(b => b.freeSpaceFollowing < blockToMove.size)
-      val leftListIncludesNextBlock = leftList.exists(b => b.id == blockToMove.id - 1)
-      
-      if (leftListIncludesNextBlock || leftList.length == blocks.length - 1)
-        // no space, move on
-        nextBlock match
-          case Some(block) =>
-            // println(s"no room for ${blockToMove.id}, trying ${block.id}")
-            moveBlocksPt2(blocks, block)
-          case None =>
-            // println(s"could not find block with id ${blockToMove.id - 1}")
-            blocks
+      if (numberOfBlocksToMove == 0)
+        // 0 blocks to move for target
+        moveBlocksPt2(blocks, target - 1)
       else
-        blocksWithoutOneToMove.drop(leftList.length) match
-          case head::rest =>
-             nextBlock match
-              case Some(block) =>
-                // println(s"moving ${blockToMove.id} beside ${head.id}. Now trying ${block.id}")
-                // update the rest
-                val preceedingBlock = blocks
-                  .sliding(2)
-                  .filter(pair => pair match
-                    case b1::b2::Nil => b2.id == blockToMove.id
-                    case _ => false
-                  )
-                  .toList
-                  .head
-                  .head
-                val withUpdatedFreeSpace = rest.filter(b => b != blockToMove).map(b => if (b.id == preceedingBlock.id) then b.copy(freeSpaceFollowing = b.freeSpaceFollowing + blockToMove.size + blockToMove.freeSpaceFollowing) else b)
-                val nextList = leftList ++ List(head.copy(freeSpaceFollowing = 0), blockToMove.copy(freeSpaceFollowing = head.freeSpaceFollowing - blockToMove.size)) ++ withUpdatedFreeSpace
-                // println(nextList)
-                moveBlocksPt2(nextList, block)
-              case None =>
-                // println(s"could not find block with id ${blockToMove.id - 1}")
-                blocks
-          case Nil =>
-            nextBlock match
-              case Some(block) =>
-                // println(s"only place for ${blockToMove.id} is at the end")
-                moveBlocksPt2(blocks, block)
-              case None =>
-                // println("End")
-                blocks
-    
+        val maybeFirstEmpty =
+          Range(0, indicesOfBlocksToMove.head + 1)
+            .find(i =>
+              Range(i, i + numberOfBlocksToMove).forall(n => blocks(n).isInstanceOf[Empty])
+            )
 
-  @tailrec
-  def addStartIndices(processed: List[(Block, Long)], toProcess: List[Block], index: Long): List[(Block, Long)] =
-    toProcess match
-      case Nil => processed
-      case head::tail =>
-        addStartIndices(processed ++ List((head, index)), tail, index + head.size + head.freeSpaceFollowing)
+        maybeFirstEmpty match
+          case None =>
+            // could not find enough contiguous empty blocks
+            moveBlocksPt2(blocks, target - 1)
+          case Some((firstSpaceAvailable)) =>
+            Range(0, numberOfBlocksToMove).foreach(i =>
+              blocks.update(firstSpaceAvailable + i, File(target))
+              blocks.update(indicesOfBlocksToMove(i), Empty())
+            )
+            moveBlocksPt2(blocks, target - 1)
 
-  def parsePt1(line: String): Array[Fragment] =
+  def parse(line: String): Array[Fragment] =
     line
       .toCharArray()
       .zipWithIndex
@@ -108,54 +88,31 @@ object Day9 {
       ).flatten
       .toArray
 
-  def parsePt2(line: String): List[Block] =
-    line
-      .toCharArray()
-      .toList
-      .grouped(2)
-      .zipWithIndex
-      .map((pair, id) =>
-        pair match
-          case fileSize::freeSpace::Nil =>
-            Block(id = id, size = fileSize.asDigit, freeSpaceFollowing = freeSpace.asDigit)
-          case fileSize::Nil => // last one
-            Block(id = id, size = fileSize.asDigit, freeSpaceFollowing = 0)
-          case _ =>
-            throw new Error(s"Unexpected data structure at index ${id * 2}")
-      )
-      .toList
-
   def totalFileBlocks(blocks: Array[Fragment]) =
     blocks.filter(b => b match
       case File(_) => true
       case Empty() => false
     ).length
 
-  def calcChecksumPt1(blocks: Array[Fragment]) =
+  def calcChecksum(blocks: Array[Fragment]) =
     blocks.zipWithIndex.map((f, i) =>
       f match
         case File(id) => i * id
         case Empty() => 0
     ).sum
 
-  def calcChecksumPt2(blocksWithIndices: List[(Block, Long)]) =
-    blocksWithIndices
-      .map(
-        (block, startIndex) => 
-          val range = startIndex to (startIndex + block.size.toLong - 1L)
-          range.map(i => i * block.id).sum
-      )
-      .sum
-
   def part1(lines: List[String]): String =
-    val parsed = parsePt1(lines.head)
+    val parsed = parse(lines.head)
     val moved = moveBlocksPt1(parsed, totalFileBlocks(parsed))
-    calcChecksumPt1(moved).toString()
+    calcChecksum(moved).toString()
 
   def part2(lines: List[String]): String =
-    val parsed = parsePt2(lines.head)
-    val moved = moveBlocksPt2(parsed, parsed.last)
-    val withIndices = addStartIndices(List.empty, moved, 0)
-    
-    calcChecksumPt2(withIndices).toString()
+    val parsed = parse(lines.head)
+    val lastFile = parsed.findLast(_.isInstanceOf[File])
+    lastFile match
+      case Some(File(id)) =>
+        val moved = moveBlocksPt2(parsed, id)
+        calcChecksum(moved).toString()
+      case _ =>
+        "no file ids found"
 }
