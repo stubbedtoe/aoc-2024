f90b1af7ef3eb599855885973e3f5de79020cdee
Andrew Healy <werdnah19@gmail.com>
2024-12-11 15:07:17 +0000

    small tidy

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -3,22 +3,22 @@ object Day11 {
     override def apply(key: I) = getOrElseUpdate(key, f(key))
   }
 
-  lazy val blink: ((Long, Int, Int)) => Long = memoize {
-    case (stone, iteration, target) =>
-      if (iteration == target)
+  lazy val blink: ((Long, Int)) => Long = memoize {
+    case (stone, countdown) =>
+      if (countdown == 0)
         1
       else
         val s = stone.toString()
         stone match
           case 0 =>
-            blink((1, iteration + 1, target))
+            blink((1, countdown - 1))
           case n if s.length % 2 == 0 =>
             val l = s.length / 2
-            val s1 = blink((s.take(l).toLong, iteration + 1, target))
-            val s2 = blink((s.drop(l).toLong, iteration + 1, target))
+            val s1 = blink((s.take(l).toLong, countdown - 1))
+            val s2 = blink((s.drop(l).toLong, countdown - 1))
             s1 + s2
           case _ =>
-            blink((stone * 2024L, iteration + 1, target))
+            blink((stone * 2024L, countdown - 1))
   }
 
   def solve(lines: List[String], part: Part): String =
@@ -29,7 +29,7 @@ object Day11 {
           case Part.Two => 75
       }
     stones
-      .map(s => blink((s, 0, iterations)))
+      .map(s => blink((s, iterations)))
       .sum
       .toString()
 }
